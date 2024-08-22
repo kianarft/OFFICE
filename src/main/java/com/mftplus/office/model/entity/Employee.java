8f25da12820acd2f4e9b8659ddf7c65431239584
Mahtab Saedi <mhtb.sd82@gmail.com>
2024-08-22 13:04:10 +0330

    employee updated..

---
+++ src/main/java/com/mftplus/office/model/entity/Employee.java
@@ -0,0 +1,33 @@
+package com.mftplus.office.model.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@SuperBuilder
+
+@Entity(name = "employeeEntity")
+@Table(name = "employee_tbl")
+
+public class Employee {
+    @Id
+    @SequenceGenerator(name = "employeeSeq", sequenceName = "employee_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "employeeSeq")
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "employee_name", length = 50)
+    private String employeeName;
+
+    @Column(name = "position_employee")
+    private String positionEmployee;
+
+    @Column(name = "hourly payment")
+    private String hourlyPayment;
+
+}
