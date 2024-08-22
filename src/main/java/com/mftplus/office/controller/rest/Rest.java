1979e60660d50d93c7ef17c5ca062c5bbdd061d8
Mahtab Saedi <mhtb.sd82@gmail.com>
2024-08-22 20:23:39 +0330

    Rest Updated..

---
+++ src/main/java/com/mftplus/office/controller/rest/Rest.java
@@ -0,0 +1,9 @@
+package com.mftplus.office.controller.rest;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/rest")
+public class Rest extends Application {
+
+}
