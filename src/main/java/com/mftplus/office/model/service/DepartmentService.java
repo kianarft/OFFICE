b918a1ae93deacedca0bb185948c3710cc69d1ba
Mahtab Saedi <mhtb.sd82@gmail.com>
2024-08-23 12:52:39 +0330

    Department Service Updated.

--- src/main/java/com/mftplus/office/model/service/DepartmentService.java
+++ src/main/java/com/mftplus/office/model/service/DepartmentService.java
@@ -1,9 +1,11 @@
 package com.mftplus.office.model.service;
 
 import com.mftplus.office.model.entity.Department;
+import com.mftplus.office.model.entity.Role;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
 
 import java.util.List;
 
@@ -12,11 +14,13 @@ public class DepartmentService {
     @PersistenceContext(unitName = "office")
     private EntityManager entityManager;
 
+    @Transactional
     public Department save(Department department) throws Exception {
         entityManager.persist(department);
         return department;
     }
 
+    @Transactional
     public Department edit(Department department) throws Exception {
         Department foundPerson = entityManager.find(Department.class, department.getId());
         if (foundPerson != null) {
@@ -25,6 +29,7 @@ public class DepartmentService {
         return department;
     }
 
+    @Transactional
     public Department remove(Long id) throws Exception {
         Department department = entityManager.find(Department.class, id);
         if (department != null) {
@@ -34,29 +39,32 @@ public class DepartmentService {
         return department;
     }
 
+    @Transactional
     public List<Department> findAll() throws Exception {
         return entityManager
                 .createQuery("select oo from departmentEntity oo where oo.deleted=false", Department.class)
                 .getResultList();
     }
 
+    @Transactional
     public Department findById(Long id) throws Exception {
         Department department = entityManager.find(Department.class, id);
         return department;
     }
 
-//    public List<Department> findByNameAndFamily(String name, String family) throws Exception {
-//        return entityManager
-//                .createQuery("select p from departmentEntity p where p.name like :name and p.family like :family", Department.class)
-//                .setParameter("name", name + "%")
-//                .setParameter("family", family + "%")
-//                .getResultList();
-//    }
-//
-//    public Department findByPhoneNumber(String phoneNumber) throws Exception {
-//        return entityManager
-//                .createQuery("select p from departmentEntity p where p.phoneNumber =:phoneNumber", Department.class)
-//                .setParameter("phoneNumber", phoneNumber)
-//                .getSingleResult();
-//    }
+    @Transactional
+    public Department findByName(String departmentName) throws Exception {
+        return entityManager
+                .createQuery("select oo from departmentEntity oo where oo.departmentName =:departmentName", Department.class)
+                .setParameter("departmentName", departmentName)
+                .getSingleResult();
+    }
+
+    @Transactional
+    public Department findByDescription(String descriptionDepartment) throws Exception {
+        return entityManager
+                .createQuery("select oo from departmentEntity oo where oo.descriptionDepartment =:descriptionDepartment", Department.class)
+                .setParameter("descriptionDepartment", descriptionDepartment)
+                .getSingleResult();
+    }
 }
