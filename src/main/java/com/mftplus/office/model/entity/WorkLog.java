81e1fe9d6bdd8aa1c9e7cd8f29dddbbd9365401a
Mahtab Saedi <mhtb.sd82@gmail.com>
2025-01-03 11:49:11 +0330

    WorkLog Entity Updated...

--- src/main/java/com/mftplus/office/model/entity/WorkLog.java
+++ src/main/java/com/mftplus/office/model/entity/WorkLog.java
@@ -2,10 +2,9 @@ package com.mftplus.office.model.entity;
 
 import jakarta.persistence.*;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
-
 import java.time.LocalDateTime;
 
 @NoArgsConstructor
